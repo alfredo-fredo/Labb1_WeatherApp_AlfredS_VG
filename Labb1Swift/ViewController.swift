50a2bde41da2b5cad7742417922fb9831aa6c34d
alfredo-fredo <alfred98.sav@gmail.com>
2020-02-18 14:57:30 +0100

    Initial Commit

---
+++ Labb1Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Labb1Swift
+//
+//  Created by Alfred on 2020-02-18.
+//  Copyright © 2020 Alfred. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
